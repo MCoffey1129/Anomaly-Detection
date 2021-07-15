20e258c1ca9dc5761912528be7596e9a887a2c88
Martin Coffey <mcoffey1129@hotmail.com>
2021-07-15 21:41:28 +0100

    Anomaly Detection 4th save

--- Anomaly_detection_code.py
+++ Anomaly_detection_code.py
@@ -1,6 +1,13 @@
+##############################################################################################################
+                                        # Anomaly Detection
+##############################################################################################################
+
+# In the attached code we will use the Iris dataset, we will relabel two Setosa entries as Versicolor
+# and check to see if the two Unsupervised learning models (K means clustering and Density based clustering method
+# (DBSCAN)) will detect the mis-labelled entries. Please note we would never assume that
+# any cases which are flagged from these models are anomalies without consulting with an expert in the field.
 
 # Import packages
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
@@ -24,24 +31,27 @@ iris.isnull().sum() # there are no null values in the data
 # setosa, versicolor and virginica.
 iris['species'].unique()
 
-# Rename the first and fourth species as versicolor
+# Rename the first and fourth species as Versicolor
+# We will check to see if the unsupervised models will flag these cases as anomalies
 iris.iloc[0,4] = 'versicolor'
 iris.iloc[3,4] = 'versicolor'
-iris.head() # the first two species have been updated
+iris.head() # the first and fourth species have been changed to Versicolor
 
-
+# Only want to look at the Versicolor flowers
 versi_df =  iris.loc[iris['species']=='versicolor']
 versi_df = versi_df.reset_index(drop=True)
 versi_df.head()
 
 # Scale the data using the MinMaxScaler
+# You should always scale your data when you are looking at distance based models
 versi_np = versi_df.iloc[:,:4].values
 scaler = MinMaxScaler()
 versi_sc = scaler.fit_transform(versi_np)
 
 
 
-# Looking at the graphs it looks like they are anomalies/misclassifications
+# Visualising the data to see if the mis-labelled cases look like anomalies
+# plt.clf()
 sns.set()
 plt.subplot(1,2,1)
 sns.scatterplot(data=versi_df, x='sepal_length', y='sepal_width')
@@ -88,6 +98,11 @@ plt.show()
 # Training the K-Means model on the dataset
 kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 2).fit(versi_sc)
 
+
+####
+# Test 1 - test to see if the anomalies are far from the cluster centroids
+####
+
 # Obtain predictions and calculate distance from cluster centroid
 versi_sc_clusters = kmeans.predict(versi_sc)
 versi_sc_clusters_centers = kmeans.cluster_centers_
@@ -102,11 +117,18 @@ km_y_pred[dist >= np.percentile(dist, 95)] = 1
 km_y_pred[dist < np.percentile(dist, 95)] = 0
 
 
-# Versicolor with the predictions
+# The anomalies flagged using distances from the centroid are not the mis-labelled cases. As you will see
+# in test 2 this is because one of the three clusters contain only the mis-labelled cases.
+
+####
+# Test 2 - Testing to see if one of the clusters contain only the mis-labelled cases
+####
+
+# Versicolor dataframe with the clusters
 versi_clus = pd.concat([versi_df,
                         pd.DataFrame(versi_sc_clusters,columns=['Clusters'])],axis=1)
 
-# We can see that a cluster containing the two incorrectly labelled....
+# We can see that one of the clusters contain only the mis-labelled cases
 # plt.clf()
 plt.subplot(1,2,1)
 sns.scatterplot(data=versi_clus, x='sepal_length', y='sepal_width', hue='Clusters', palette='deep')
@@ -141,11 +163,12 @@ pred_labels = db.labels_
 n_clusters = len(set(pred_labels)) - (1 if -1 in pred_labels else 0)
 
 # Print performance metrics for DBSCAN
+# There are only two clusters
 print('Estimated number of clusters: %d' % n_clusters)
 
 
 versi_db = pd.concat([versi_clus,
                         pd.DataFrame(pred_labels,columns=['db_cluster'])],axis=1)
 
-
-versi_db.head() # Again it is the first two cases that are being flagged as anomalies
+# Under both DBSCAN and K means clustering the first two cases are being flagged as anomalies
+versi_db.head()
